136015adf83275a871908805652f923630f5a68e
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2023-09-23 12:55:10 +0630

    Sites: changed Name.

--- _data/sites/ksvblog.js
+++ _data/sites/ksvblog.js
@@ -1,13 +1,12 @@
 module.exports = {
-  name: "K's V/Blog", // optional, falls back to object key
+  name: "ksvblog",
   description: "K's V/Blog Personal Websites",
   options: {
     runs: 1,
-    frequency: 1, // (in minutes)
+    frequency: 1,
   },
   urls: [
     "https://ksvblog.site/",
-    "https://ksvblog.github.io/",
     "https://ksvblog.netlify.app/",
     "https://ksvblog.pages.dev/",
   ],
