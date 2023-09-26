57fef35ed4bd69e245db415b10bb4f9e587b880b
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2023-09-26 11:16:02 +0630

    URL: stats, speedlify Updated.

--- _data/sites/ksvblog.js
+++ _data/sites/ksvblog.js
@@ -1,15 +1,20 @@
 module.exports = {
   name: "ksvblog",
   description: "K's V/Blog Personal Websites",
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
-	options: {
-		frequency: 60 * 23, // 23 hours
-		freshChrome: "site"
-	},
+  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+  options: {
+    frequency: 60 * 12, // 23 hours
+    freshChrome: "site",
+  },
   urls: [
     "https://ksvblog.site/",
     "https://ksvblog.github.io/",
     "https://ksvblog.netlify.app/",
     "https://ksvblog.pages.dev/",
+    // Website Uptime.
+    "https://stats.ksvblog.site/",
+    // Benchmark K's V/Blog Personal Websites over time.
+    "https://speedlify.ksvblog.site/",
+    "https://ksvspeedlify.netlify.app",
   ],
 };
