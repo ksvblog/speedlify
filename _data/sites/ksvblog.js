7ef4f5fad90ed62021ad720bb330510dfd6df60c
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2023-09-26 11:06:02 +0630

    Update ksvblog.js

--- _data/sites/ksvblog.js
+++ _data/sites/ksvblog.js
@@ -1,10 +1,11 @@
 module.exports = {
   name: "ksvblog",
   description: "K's V/Blog Personal Websites",
-  options: {
-    runs: 1,
-    frequency: 1,
-  },
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		freshChrome: "site"
+	},
   urls: [
     "https://ksvblog.site/",
     "https://ksvblog.netlify.app/",
